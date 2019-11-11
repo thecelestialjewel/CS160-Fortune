6094ae8ad07e0e871ea1ac79c832dc078667262a
thecelestialjewel <christinemjordan6@gmail.com>
2019-11-10 16:03:50 -0800

    Initial team project checkin
    Project renamed to Fortune

---
+++ Fortune/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSelectNumber = new System.Windows.Forms.Label();
+            this.numSelector = new System.Windows.Forms.NumericUpDown();
+            this.btnClickForFortune = new System.Windows.Forms.Button();
+            this.btnLuckyNumbers = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numSelector)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblSelectNumber
+            // 
+            this.lblSelectNumber.AutoSize = true;
+            this.lblSelectNumber.Location = new System.Drawing.Point(39, 40);
+            this.lblSelectNumber.Name = "lblSelectNumber";
+            this.lblSelectNumber.Size = new System.Drawing.Size(111, 13);
+            this.lblSelectNumber.TabIndex = 0;
+            this.lblSelectNumber.Text = "Select a number 0-21:";
+            // 
+            // numSelector
+            // 
+            this.numSelector.Location = new System.Drawing.Point(172, 38);
+            this.numSelector.Name = "numSelector";
+            this.numSelector.Size = new System.Drawing.Size(136, 20);
+            this.numSelector.TabIndex = 2;
+            this.numSelector.ValueChanged += new System.EventHandler(this.numSelector_ValueChanged);
+            // 
+            // btnClickForFortune
+            // 
+            this.btnClickForFortune.Location = new System.Drawing.Point(42, 103);
+            this.btnClickForFortune.Name = "btnClickForFortune";
+            this.btnClickForFortune.Size = new System.Drawing.Size(266, 80);
+            this.btnClickForFortune.TabIndex = 3;
+            this.btnClickForFortune.Text = "Click button for your fortune";
+            this.btnClickForFortune.UseVisualStyleBackColor = true;
+            this.btnClickForFortune.Click += new System.EventHandler(this.btnClickForFortune_Click);
+            // 
+            // btnLuckyNumbers
+            // 
+            this.btnLuckyNumbers.Location = new System.Drawing.Point(98, 232);
+            this.btnLuckyNumbers.Name = "btnLuckyNumbers";
+            this.btnLuckyNumbers.Size = new System.Drawing.Size(141, 23);
+            this.btnLuckyNumbers.TabIndex = 4;
+            this.btnLuckyNumbers.Text = "Click for lucky numbers";
+            this.btnLuckyNumbers.UseVisualStyleBackColor = true;
+            this.btnLuckyNumbers.Click += new System.EventHandler(this.btnLuckyNumbers_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 316);
+            this.Controls.Add(this.btnLuckyNumbers);
+            this.Controls.Add(this.btnClickForFortune);
+            this.Controls.Add(this.numSelector);
+            this.Controls.Add(this.lblSelectNumber);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numSelector)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSelectNumber;
+        private System.Windows.Forms.NumericUpDown numSelector;
+        private System.Windows.Forms.Button btnClickForFortune;
+        private System.Windows.Forms.Button btnLuckyNumbers;
+    }
+}
+
---
+++ Fortune/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public int selectedNum { get; private set; }
+
+        //string fortuneMessage;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void numSelector_ValueChanged(object sender, EventArgs e)//handling with this method
+        {
+            try
+            {
+                NumericUpDown selector = (NumericUpDown)sender;//send this information to the event handler
+                selectedNum = (int)selector.Value;
+
+                if (selectedNum > 22 && selectedNum < 0)
+                {
+                    throw new Exception("Oh snap! You need to be a number between 0-21");
+                }
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "You screwed up!");
+                
+            }
+        }
+        private void btnClickForFortune_Click(object sender, EventArgs e)
+        {
+ 
+            string[] fortuneMessage = new string[23];// string array for fortune message
+
+            string card = "You've got the ";
+            //https://www.tarot.com/tarot/cards/major-arcana
+            fortuneMessage[0] = card + "Fool card:\n\n Beginnings, innocence, spontaneity, a free spirit";
+            fortuneMessage[1] = card + "Magician card: Everything you need right now is at your fingertips. You are ready to turn your ideas into reality. Establish a clear vision of what will create before you act. Have clear goals that talk to your soul";
+            fortuneMessage[2] = card + "High Priestess card: Connect with your intuition through meditation, visualization, journeying, and being part of a spiritual community. Look for areas in your life that may be out of balance and trust your intuition to correct them";
+            fortuneMessage[3] = card + "Empress card: Connect with your senses to experience a deep fulfillment. Discover different approaches to express yourself creatively through painting, music, drama, or other fine arts. You are in a period of growth, in which all you have dreamed of is now coming true";
+            fortuneMessage[4] = card + "The Emperor: You are comfortable in a leadership role where you can direct others. Listen to the advice of others, but you need to have the final say. Create calm out of chaos by breaking down  problems and using a systematic approach to solve them";
+            fortuneMessage[5] = card + "The Hierophant: You will undertake a period of formal study as you delve into a subject that has been widely documented. You adhere to key principles and rules that you know will lead to a successful result. You enjoy a deep sense of comfort being surrounded by people with well established belief systems";
+            fortuneMessage[6] = card + "The Lovers: Be vulnerable, open your heart, and share your truest feelings. You will create a harmonious and fulfilling relationship built on trust and respect. It is time to go into the world and make choices for yourself, staying true to who you are and being authentic in your endeavors";
+            fortuneMessage[7] = card + "The Chariot: Don’t be passive and just hope things will work in your favor. Take action and stay on course regardless of what challenges you will face. When others will try to distract you from your goal, draw upon your willpower. You will be successful as long as your remain confident in your abilities";
+            fortuneMessage[8] = card + "Strength: You are fueled by your strong will and determination. Your strength gives you the confidence to overcome challenges and fears. Tame your animal instincts and channel these responses constructively. Its what you do with emotions that makes a difference";
+            fortuneMessage[9] = card + "The Hermit: Your most profound sense of truth is within you and not in the distractions of the outside world. When you allow yourself to tune into your inner guiding light you will hear the answers you need to grow wise beyond your years";
+            fortuneMessage[10] = card + "Wheel of Fortune: Be kind and loving to others and they will be kind and loving to you. If you want happiness and abundance make sure you are sending out positive juju. What you send out in the universe will come back your way. You need to accept what is happening and adapt. Go with the flow";
+            fortuneMessage[11] = card + "Justice: ";
+            fortuneMessage[12] = card + "The Hanged Man: ";
+            fortuneMessage[13] = card + "Death: ";
+            fortuneMessage[14] = card + "Temperance ";
+            fortuneMessage[15] = card + ": ";
+            fortuneMessage[16] = card + ": ";
+            fortuneMessage[17] = card + ": ";
+            fortuneMessage[18] = card + ": ";
+            fortuneMessage[19] = card + ": ";
+            fortuneMessage[20] = card + ": ";
+            fortuneMessage[21] = card + ": ";
+            fortuneMessage[22] = card + ": ";
+            
+           
+
+            {
+                MessageBox.Show("Your fortune: \n\n"
+                                    + fortuneMessage[selectedNum]);
+                
+            }
+        }
+
+        private void btnLuckyNumbers_Click(object sender, EventArgs e)
+        {
+            int[] luckynumbers = GenerateLuckyNumbers();
+
+            
+            string commaseperatorluckynumber = string.Join(", ", luckynumbers);
+            MessageBox.Show("Your lucky numbers: " + commaseperatorluckynumber);
+
+
+            }
+        public static int [] GenerateLuckyNumbers()
+        {
+            int[] luckyNumbers = new int[6]; //int array for lucky numbers
+            for (int i = 0; i < 6; i++)
+            {
+                Random rnd = new Random();
+                luckyNumbers[i] = rnd.Next(1, 49);//random numbers 1 through 49
+            }
+            return luckyNumbers;
+
+        }
+    }
+}
